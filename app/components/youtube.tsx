8abf285dc755904e21c754fa73f1221b40cafc6e
Tyler England <tyler_england35@yahoo.com>
2024-10-13 11:54:43 -0700

    Tesitng precommit

--- app/components/youtube.tsx
+++ app/components/youtube.tsx
@@ -6,8 +6,8 @@ export function YouTubeComponent(props: any) {
     <div className="relative w-full h-0 pb-[56.25%] my-6">
       <YT
         opts={{
-          height: '100%',
-          width: '100%',
+          height: "100%",
+          width: "100%",
         }}
         {...props}
         className="absolute top-0 left-0 w-full h-full"
