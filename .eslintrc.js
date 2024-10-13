b64bfe86e84612ba465390d51c20f5ff6296550f
Tyler England <tyler_england35@yahoo.com>
2024-10-13 11:11:50 -0700

    Husky set up for linting stage files

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier'
+    ],
+    plugins: ['@typescript-eslint', 'react'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
+
