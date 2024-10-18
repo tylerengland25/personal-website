374a988d83aa2378cd5e28196c32c7748913b63e
Tyler England <tyler_england35@yahoo.com>
2024-10-17 20:49:09 -0700

    Reverse grayscale hover

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         <Image
           src="/photos/profile-photo.png"
           alt="Profile photo"
-          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
+          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 hover:grayscale"
           unoptimized
           width={160}
           height={160}
