61dc2688736cfabe7b43c05c318235b46622ece0
Tyler England <tyler_england35@yahoo.com>
2024-11-23 16:15:35 -0800

    Moved blogs inside of blog folder

--- app/lib/posts.ts
+++ app/lib/posts.ts
@@ -7,6 +7,8 @@ type Metadata = {
   summary: string;
   tags: string;
   image?: string;
+  thumbnail?: string;
+  tag?: string;
 };
 
 function parseFrontmatter(fileContent: string) {
@@ -51,7 +53,7 @@ function getMDXData(dir: string) {
 }
 
 export function getBlogPosts() {
-  return getMDXData(path.join(process.cwd(), 'content'));
+  return getMDXData(path.join(process.cwd(), 'app/blog/blogs'));
 }
 
 export function formatDate(date: string, includeRelative = false) {
