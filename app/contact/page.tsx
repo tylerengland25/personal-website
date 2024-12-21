09deccea38c00127632e1cd1be89cf9c6068133a
Tyler England <tyler_england35@yahoo.com>
2024-12-20 21:35:22 -0800

    Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,17 +4,23 @@ import { socialLinks } from '../config';
 import { Navbar } from '../(home)/components/nav';
 import Footer from '../(home)/components/footer';
 import TypeWriter from '../components/type-writer';
+import MobileNav from '../(home)/components/mobile-nav';
 
 export default function Contact() {
   return (
     <div className="h-screen overflow-hidden relative flex flex-col">
       <Background imagePath="/backgrounds/desk-setup-dark.webp" />
       <div className="fixed top-0 left-0 right-0 z-50 bg-transparent">
-        <Navbar />
+        <div className="lg:block hidden">
+          <Navbar />
+        </div>
+        <div className="lg:hidden">
+          <MobileNav />
+        </div>
       </div>
       <div className="flex-1 flex flex-col">
-        <div className="fixed top-[200px] left-0 right-0 z-40">
-          <section className="max-w-6xl mx-auto px-4 relative">
+        <div className="fixed top-[150px] lg:top-[200px] left-0 right-0 z-40 overflow-y-auto bottom-16">
+          <section className="max-w-6xl mx-auto px-4 relative pb-8">
             <div className="mb-12">
               <h1 className="text-4xl font-bold mb-4 text-white">
                 Get in Touch
